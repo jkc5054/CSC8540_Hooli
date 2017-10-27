e1c8d00780e477578fcd6f22f2845d21ce7ee6ef
priyaprasad27 <pprasad@villanova.edu>
2017-10-26 20:21:09 -0400

    test

--- src/MVC/HuskerDuModel.java
+++ src/MVC/HuskerDuModel.java
@@ -21,7 +21,7 @@ public class HuskerDuModel {
 	
 	public void LoadImages()
 	{
-		File folder = new File("C:\\github-repos\\Hooli\\cfg\\GameImages");
+		File folder = new File("C:\\github-repos\\CSC8540_Hooli\\cfg\\GameImages");
 		ArrayList<File> listOfFiles = new ArrayList<File>(Arrays.asList(folder.listFiles()));
 		ArrayList<ImageIconContainer> listOfContainers = new ArrayList<ImageIconContainer>();
 		
@@ -44,7 +44,7 @@ public class HuskerDuModel {
 		arrayOfContainers = listOfContainers.toArray(new ImageIconContainer[listOfContainers.size()]);		
 		
 		try {
-			defaultIcon = new ImageIcon(ImageIO.read(new File("C:\\github-repos\\Hooli\\cfg\\DefaultIcon.png")));
+			defaultIcon = new ImageIcon(ImageIO.read(new File("C:\\github-repos\\CSC8540_Hooli\\cfg\\DefaultIcon.png")));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
